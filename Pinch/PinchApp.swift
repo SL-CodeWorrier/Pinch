5f6f9d3246b0ef9b33e010e6ae53ce089962202d
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-02-06 11:43:29 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Chathura Aththanayaka on 2024-02-06.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
